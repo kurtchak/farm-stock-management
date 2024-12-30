7153ad37e704e03ee367f9fd60171afa5d39a683
Ján Korčák <jan.korcak@external.telekom.sk>
2024-12-30 07:31:50 +0100

    implement classes

--- backend/src/main/java/com/farmstock/FarmStockApplication.java
+++ backend/src/main/java/com/farmstock/FarmStockApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {"com.farmstock.model"})
-@EnableJpaRepositories(basePackages = {"com.farmstock.repository"})
+@EntityScan(basePackages = "com.farmstock.model")
+@EnableJpaRepositories(basePackages = "com.farmstock.repository")
 public class FarmStockApplication {
     public static void main(String[] args) {
         SpringApplication.run(FarmStockApplication.class, args);
