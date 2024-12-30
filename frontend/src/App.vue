2ea5a1fb9dd1b59e0a7e0e8cebf5ef38c9611fef
Ján Korčák <jan.korcak@external.telekom.sk>
2024-12-30 09:29:42 +0100

    implement classes

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,8 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <TestComponent></TestComponent>
   </div>
 </template>
 
 <script setup>
-import TestComponent from "./components/test/TestComponent.vue";
 </script>
