e84273702189c831a9674ed419c5c267eeb49254
Ján Korčák <jan.korcak@external.telekom.sk>
2025-01-03 21:00:35 +0100

    Adjust timeouts to debug prod connectivity

--- backend/src/main/java/com/farmstock/controller/TestController.java
+++ backend/src/main/java/com/farmstock/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.farmstock.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
 
     @GetMapping("/test")
-    public String test() {
-        return "Backend is working!";
+    public ResponseEntity<String> test() {
+        return ResponseEntity.ok("API is working");
     }
 }
